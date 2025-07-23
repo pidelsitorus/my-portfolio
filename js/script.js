c399e4f451b895e3132e5377449ad7d1ed9060a3
pidelsitorus <bataranadapdap34@gmail.com>
2025-07-23 14:50:45 +0700

    update

--- js/script.js
+++ js/script.js
@@ -6,11 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
     window.onscroll = function() {
         const header = document.querySelector('header');
         const fixedNav = header.offsetTop;
+        const toTop = document.querySelector('#to-top');
 
         if (window.pageYOffset > fixedNav) {
             header.classList.add('navbar-fixed');
+            toTop.classList.remove('hidden');
+            toTop.classList.add('flex');
         } else {
             header.classList.remove('navbar-fixed');
+            toTop.classList.remove('flex');
+            toTop.classList.add('hidden');
         }
     }
 });
@@ -22,4 +27,25 @@ const NavMenu = document.querySelector('#nav-menu');
 hamburger.addEventListener('click', function()    {
     hamburger.classList.toggle('hamburger-active');
     NavMenu.classList.toggle('hidden');
+});
+
+
+// klik diluar hamburger
+window.addEventListener('click', function(event) {
+    if (!hamburger.contains(event.target) && !NavMenu.contains(event.target)) {
+        hamburger.classList.remove('hamburger-active');
+        NavMenu.classList.add('hidden');
+    }
+});
+
+// Dark Mode Toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click', function() {
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+    } else {
+        html.classList.remove('dark');
+    }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   safelist: ['hamburger-line'],
   theme: {
     extend: {
